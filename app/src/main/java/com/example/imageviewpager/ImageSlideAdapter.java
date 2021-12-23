55347aa71d4cfcec89abdf11b0a32a8ca47ee068
김창배 <cbkim93@naver.com>
2021-12-23 23:32:13 +0900

    이미지 슬라이드  - 각 버튼으로 이미지 슬라이드 뷰에 이미지 개별 업로드  - 저장 버튼으로 저장할 이미지 획득

--- app/src/main/java/com/example/imageviewpager/ImageSlideAdapter.java
+++ app/src/main/java/com/example/imageviewpager/ImageSlideAdapter.java
@@ -2,6 +2,7 @@ package com.example.imageviewpager;
 
 import android.content.Context;
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,19 +13,35 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
+import java.util.ArrayList;
+
 public class ImageSlideAdapter extends PagerAdapter {
 
-    private int[] images = {R.drawable.image1,R.drawable.image2,R.drawable.image3};
+    private ArrayList<Integer> imageList;
     private LayoutInflater  inflater;
     private Context         context;
 
     public ImageSlideAdapter(Context context) {
         this.context = context;
+        imageList = new ArrayList<>();
+    }
+
+    public ArrayList<Integer> getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(ArrayList<Integer> imageList) {
+        this.imageList = imageList;
+    }
+
+    public void setImage(int image) {
+        imageList.add(image);
     }
 
     @Override
     public int getCount() {
-        return images.length;
+        Log.d("어댑터", "어댑터 사이즈 : " + imageList.size());
+        return imageList.size();
     }
 
     @Override
@@ -35,14 +52,19 @@ public class ImageSlideAdapter extends PagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        Log.d("어댑터", "instantiateItem : " + position);
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = inflater.inflate(R.layout.item_image_slide, container, false);
+
         ImageView imageView = v.findViewById(R.id.image);
         TextView textView = v.findViewById(R.id.txtTitle);
+        TextView count = v.findViewById(R.id.txtImageCount);
 
-        imageView.setImageResource(images[position]);
+        imageView.setImageResource(imageList.get(position));
         String text = (position + 1) + " 번째 이미지";
         textView.setText(text);
+
+        count.setText(imageList.size() + " 장");
 
         container.addView(v);
         return v;
--- app/src/main/java/com/example/imageviewpager/MainActivity.java
+++ app/src/main/java/com/example/imageviewpager/MainActivity.java
@@ -4,19 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager           viewPager;
     private ImageSlideAdapter   adapter;
 
+    private Button  upload1, upload2, upload3, btnSave;
+    private ArrayList<Integer> imageList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setEvents();
+        viewPager.setAdapter(adapter);
+    }
+
+    private void init() {
+        upload1 = findViewById(R.id.btnImageUpload1);
+        upload2 = findViewById(R.id.btnImageUpload2);
+        upload3 = findViewById(R.id.btnImageUpload3);
+        btnSave = findViewById(R.id.btnSave);
 
         viewPager = findViewById(R.id.viewPager);
+
         adapter = new ImageSlideAdapter(this);
-        viewPager.setAdapter(adapter);
+    }
+
+    private void setEvents() {
+        btnSave.setOnClickListener(view -> {
+            // imageList = new ArrayList<>();
+            imageList = adapter.getImageList();
+            for (int i = 0; i<imageList.size(); i++) {
+                Log.d("메인", "이미지 -> " + imageList.get(i));
+            }
+        });
+
+        upload1.setOnClickListener(view -> {
+            adapter.setImage(R.drawable.image1);
+            viewPager.setAdapter(adapter);
+        });
+
+        upload2.setOnClickListener(view -> {
+            adapter.setImage(R.drawable.image2);
+            viewPager.setAdapter(adapter);
+        });
+
+        upload3.setOnClickListener(view -> {
+            adapter.setImage(R.drawable.image3);
+            viewPager.setAdapter(adapter);
+        });
     }
 }
